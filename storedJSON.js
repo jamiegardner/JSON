88f4d7d0cadec0a5d133953e8c278185bdf7fb4c
Jamie Gardner <jagardner4@uclan.ac.uk>
2018-03-03 15:34:33 +0000

    Inital commit

---
+++ storedJSON.js
@@ -0,0 +1,10 @@
+var JSONObject= {
+    "name":"Jamie Gardner",
+    "street":"167 Sherman Street",
+    "age":22,
+    "email":"jagardner4@uclan.ac.uk"};
+
+document.getElementById("name").innerHTML=JSONObject.name;
+document.getElementById("age").innerHTML=JSONObject.age;
+document.getElementById("street").innerHTML=JSONObject.street;
+document.getElementById("email").innerHTML=JSONObject.email;
