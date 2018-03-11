fb396947e14e60318f84f5eeb328cff1132f611b
Jamie Gardner <jagardner4@uclan.ac.uk>
2018-03-11 23:21:50 +0000

    comments

--- storedJSON.js
+++ storedJSON.js
@@ -1,10 +1,10 @@
 var JSONObject= {
-    "name":"Jamie Gardner",
-    "street":"167 Ben Sherman Street",
-    "age":22,
-    "email":"jagardner4@uclan.ac.uk"};
+    "name":"Jamie Gardner",                 <!-- storing the info -->
+    "street":"167 Ben Sherman Street",      <!-- storing the info -->
+    "age":"22",                             <!-- storing the info -->
+    "email":"jagardner4@uclan.ac.uk"};      <!-- storing the info -->
 
-document.getElementById("name").innerHTML=JSONObject.name;
-document.getElementById("age").innerHTML=JSONObject.age;
-document.getElementById("street").innerHTML=JSONObject.street;
-document.getElementById("email").innerHTML=JSONObject.email;
+document.getElementById("name").innerHTML=JSONObject.name; <!-- placing info in HTML document -->
+document.getElementById("age").innerHTML=JSONObject.age;   <!-- placing info in HTML document -->
+document.getElementById("street").innerHTML=JSONObject.street;  <!-- placing info in HTML document -->
+document.getElementById("email").innerHTML=JSONObject.email;    <!-- placing info in HTML document -->
